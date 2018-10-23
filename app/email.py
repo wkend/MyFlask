78e00d3c30a25de2b5d2368197b1a8a86e79f38c
wkend <37563697+wkend@users.noreply.github.com>
2018-10-24 00:00:44 +0800

    repaired some email bugs,,,,

--- app/email.py
+++ app/email.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python 3.5
-# -*- coding: utf-8 -*-
-# @Time    : 2018/10/15 23:55
-# @Author  : wkend
-# @File    : email.py
-# @Software: PyCharm
-
 # 异步发送电子邮件
 from threading import Thread
 from flask import render_template
 from flask_mail import Message
 import app
 from app import mail
-
+import smtplib
+from email.mime.text import MIMEText
 
 def send_asyc_email(app, msg):
     with app.app_context():
@@ -22,13 +16,47 @@ def send_asyc_email(app, msg):
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
 # 发件人地址
 app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <wkend@qq.com>'
+# 开启邮件发送异常模块的使用
+app.config['MAIL_USE_SSL'] = True
+
+# def send_email(to, subject, template, **kwargs):
+#     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+#                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+#     msg.body = render_template(template + '.txt', **kwargs)
+#     msg.html = render_template(template + '.html', **kwargs)
+#     t = Thread(target=send_asyc_email, args=[app, msg])
+#     t.start()
+#     return t
 
 
-def send_email(to, subject, template, **kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
-    msg.body = render_template(template + '.txt', **kwargs)
-    msg.html = render_template(template + '.html', **kwargs)
-    t = Thread(target=send_asyc_email, args=[app, msg])
-    t.start()
-    return t
+def send_email(recipients, subject, template, **kwargs):
+    """
+    发送邮件
+    :param recipients:
+    :param subject:
+    :param template:
+    :param kwargs:
+    :return:
+    """
+    content = '这是确认邮箱，欢迎来到flask blog!'
+    msg = MIMEText(content)
+    msg['Subject'] = app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject
+    msg['body'] = render_template(template + '.txt', **kwargs)
+    msg['html'] = render_template(template + '.html', **kwargs)
+
+
+    s = smtplib.SMTP_SSL("smtp.qq.com",465) # 配置邮件发送服务器
+    try:
+        s.login(app.config['FLASK_ADMIN_SENDER'],app.config['FLASK_ADMIN_AUTHORIZATION_CODE'])
+        s.sendmail(app.config['FLASK_ADMIN_SENDER'],recipients,msg.as_string())
+        print('发送成功！')
+        t = Thread(target=send_asyc_email, args=[app, msg])
+        t.start()
+        return t
+    except smtplib.SMTPException as e:
+        print('发送失败，，，')
+    finally:
+        s.quit()
+
+
+
--- config.py
+++ config.py
@@ -8,8 +8,10 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <1394059601@qq.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SENDER = 'Flasky Admin <wkend@qq.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')   # 收件人
+    FLASK_ADMIN_SENDER = os.environ.get('FLASK_ADMIN_SENDER')   # 用于发送确认邮件的发送人
+    FLASK_ADMIN_AUTHORIZATION_CODE = os.environ.get('MAIL_AUTHORIZATION_CODE')  # qq邮箱用于发送邮件的授权码
 
     @staticmethod
     def init_app(app):
@@ -21,7 +23,7 @@ class DevelopmentConfig(Config):
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_AUTHORIZATION_CODE = os.environ.get('MAIL_AUTHORIZATION_CODE')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 class TestingConfig(Config):
